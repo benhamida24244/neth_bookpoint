6a10c8434b2c88b5329edcd477ea1ba673c7fac4
Benhamida Mohammed <mbenhamida136@gmail.com>
2025-07-07 19:10:47 +0100

    Header for Mobile => (Done), add Icon with primeicons and use motion from vueMotion

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { MotionPlugin } from '@vueuse/motion'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(MotionPlugin)
 app.mount('#app')
