de40e19503b46695623f33dfa9d1d8a136c90f0a
Benhamida Mohammed <mbenhamida136@gmail.com>
2025-07-07 14:49:17 +0100

    ##header desktop(add wallpaper in header and change font header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,8 @@ import Logo from '../assets/HomeIcon/Header/Logo.png'
 
 <template>
   <header
-    class="flex shadow-md py-4 px-4 sm:px-10 bg-black min-h-[70px] tracking-wide relative z-50"
+    id="header"
+    class="flex shadow-md py-4 px-4 sm:px-10  min-h-[70px] tracking-wide relative z-50"
   >
     <div class="flex flex-wrap items-center justify-between gap-5 w-full">
       <a href="javascript:void(0)" class="max-sm:hidden">
@@ -14,7 +15,7 @@ import Logo from '../assets/HomeIcon/Header/Logo.png'
         </div>
       </a>
       <a href="javascript:void(0)" class="hidden max-sm:block">
-        <img src="https://readymadeui.com/readymadeui-short.svg" alt="logo" class="w-9"/>
+        <img :src="Logo" alt="logo" class="w-12"/>
       </a>
 
       <div
@@ -42,7 +43,7 @@ import Logo from '../assets/HomeIcon/Header/Logo.png'
         </button>
 
         <ul
-          class="lg:flex gap-x-4 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50"
+          class="font-BonaRegular lg:flex gap-x-4 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50"
         >
           <li class="mb-6 hidden max-lg:block">
             <a href="javascript:void(0)"
@@ -106,8 +107,8 @@ import Logo from '../assets/HomeIcon/Header/Logo.png'
           Sign up
         </button>
 
-        <button id="toggleOpen" class="lg:hidden cursor-pointer">
-          <svg class="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <button id="toggleOpen" class="lg:hidden cursor-pointer text-yellow-600">
+          <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path
               fill-rule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
